0b7a15f813442fcb946dce66a201f6400c3d626a
Daniel Faria Sampaio <daniel.faria.sampaio@gmail.com>
2019-12-02 23:03:40 -0300

    Removes Realm from project's dependencies

--- app/src/main/java/com/danfsd/weatherforecastapp/WeatherForecastApplication.java
+++ app/src/main/java/com/danfsd/weatherforecastapp/WeatherForecastApplication.java
@@ -2,24 +2,10 @@ package com.danfsd.weatherforecastapp;
 
 import android.app.Application;
 
-import io.realm.Realm;
-import io.realm.RealmConfiguration;
 
 public class WeatherForecastApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        setupRealm();
-    }
-
-    /**
-     * Initializes Realm default instance
-     */
-    private void setupRealm() {
-        Realm.init(this);
-        RealmConfiguration config = new RealmConfiguration.Builder()
-                .name("weather.realm")
-                .build();
-        Realm.setDefaultConfiguration(config);
     }
 }
